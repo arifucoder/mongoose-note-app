a8a80f562a7ede34206fbd9eca4c66a7614666fa
arifucoder <arifucoder@gmail.com>
2026-09-15 10:40:03 +0600

    feat: add mongoose schema and model

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import express from "express";
 import type { Application, Request, Response } from "express";
-
+import "dotenv/config";
+import { model, Schema } from "mongoose";
 const app: Application = express();
+
+const noteSchema = new Schema({
+	title: String,
+	content: String,
+});
+
+const Note = model("Note", noteSchema);
+
+app.get("/", (req: Request, res: Response) => {
+	res.json({
+		success: true,
+		message: "Welcome to mongoose note app",
+	});
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import type { Server } from "http";
 import app from "./app.js";
+import mongoose from "mongoose";
 
 let server: Server;
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 async function main() {
 	try {
+		await mongoose.connect(process.env.MONGODB_URL as string);
+		console.log("connected to database mongodb using mongoose");
 		server = app.listen(PORT, () => {
 			console.log(`App is listening on port ${PORT}`);
 		});
 	} catch (error) {
-		console.log(error);
+		console.error("Failed to connect to database:", error);
+		process.exit(1); // এটা যোগ করা ভালো
 	}
 }
 
