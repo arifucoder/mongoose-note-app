7ed61b9375cbfd95f4a9baf0d5ac6b89000da4e2
arifucoder <arifucoder@gmail.com>
2026-09-16 13:23:43 +0600

    feat: add notes get api

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,53 @@
 import express from "express";
 import type { Application, Request, Response } from "express";
 import "dotenv/config";
-import { model, Schema } from "mongoose";
+import { model, now, Schema } from "mongoose";
 
 const app: Application = express();
 app.use(express.json());
 
 const noteSchema = new Schema({
 	title: { type: String, required: true, trim: true },
-	content: String,
+	content: { type: String, default: "" },
+	category: {
+		type: String,
+		enum: ["Personal", "Work", "Other"],
+		default: "Personal",
+	},
+	pinned: {
+		type: Boolean,
+		default: false,
+	},
+	date: { type: Date, default: Date.now },
+	tags: {
+		label: { type: String, required: true },
+		color: { type: String, default: "green" },
+	},
 });
 
 const Note = model("Note", noteSchema);
 
-app.post("/create-note", async (req: Request, res: Response) => {
+app.post("/notes/create-note", async (req: Request, res: Response) => {
 	try {
-		const { title, content } = req.body;
-		const myNote = new Note({
-			title,
-			content,
-		});
+		const body = req.body;
+		const note = await Note.create(body);
 
-		await myNote.save();
-
-		res.json({
+		res.status(201).json({
 			success: true,
-			note: myNote,
+			note,
 		});
 	} catch (error: any) {
 		console.log(error.message);
 	}
+});
+
+app.get("/notes", async (req, res) => {
+	const notes = await Note.find();
+
+	res.status(200).json({
+		success: true,
+		notes,
+	});
 });
 
 app.get("/", (req: Request, res: Response) => {
